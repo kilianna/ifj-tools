7673ffc9e4f8d255c725f330adc03aa8b64e9aff
kildom <kildom@users.noreply.github.com>
2024-08-16 19:38:31 +0200

    PointGenerator: Prevent overlapping dots

--- src/io/github/kildot/ifjTools/Points_Generator.java
+++ src/io/github/kildot/ifjTools/Points_Generator.java
@@ -7,14 +7,13 @@ import ij.process.*;
 import java.awt.*;
 import java.util.*;
 
-
 public class Points_Generator implements PlugIn {
 
-        private double[] numbers;
-        private double[] averages;
-        private double radius;
-        private boolean calcAverage;
-        private long globalRandSeed;
+    private double[] numbers;
+    private double[] averages;
+    private double radius;
+    private boolean calcAverage;
+    private long globalRandSeed;
 
     @Override
     public void run(String arg) {
@@ -27,11 +26,16 @@ public class Points_Generator implements PlugIn {
         }
         Random rnd = new Random(globalRandSeed);
         for (int i = 0; i < count; i++) {
-            long seed = rnd.nextLong();
-            ImageProcessor ip = stack != null ? stack.getProcessor(i + 1) : sourceImage.getProcessor();
-            processImage(ip, seed, false);
-            if (calcAverage) {
-                processImage(ip, seed, true);
+            try {
+                long seed = rnd.nextLong();
+                ImageProcessor ip = stack != null ? stack.getProcessor(i + 1) : sourceImage.getProcessor();
+                processImage(ip, seed, false);
+                if (calcAverage) {
+                    processImage(ip, seed, true);
+                }
+            } catch (OutOfAreaException ex) {
+                IJ.log("Cannot fit all dots inside the image.");
+                IJ.log("Not all dots were generated.");
             }
         }
         String info = "";
@@ -66,121 +70,134 @@ public class Points_Generator implements PlugIn {
                 return false;
             }
         }
-                try {
-                        radius = Double.parseDouble(vect.get(1).getText());
-                        if (radius < 2 || radius > 100) throw new Exception();
-                } catch (Exception ex) {
-                        IJ.log("Invalid radius");
-                        return false;
-                }
-                Vector<Checkbox> boxes = dialog.getCheckboxes();
-                calcAverage = boxes.get(0).getState();
-                try {
-                    globalRandSeed = Long.parseLong(vect.get(2).getText());
-                } catch (NumberFormatException ex) {
-                    IJ.log("Invalid random seed");
-                    return false;
-                }
-                return true;
+        try {
+            radius = Double.parseDouble(vect.get(1).getText());
+            if (radius < 2 || radius > 100) {
+                throw new Exception();
+            }
+        } catch (Exception ex) {
+            IJ.log("Invalid radius");
+            return false;
+        }
+        Vector<Checkbox> boxes = dialog.getCheckboxes();
+        calcAverage = boxes.get(0).getState();
+        try {
+            globalRandSeed = Long.parseLong(vect.get(2).getText());
+        } catch (NumberFormatException ex) {
+            IJ.log("Invalid random seed");
+            return false;
+        }
+        return true;
     }
 
-    private void processImage(ImageProcessor ip, long seed, boolean useAverage) {
-            Random rnd = new Random(seed);
-            int width = ip.getWidth();
-            int height = ip.getHeight();
-            double startX = Math.max(width / 20, 2 * radius);
-            double startY = Math.max(height / 20, 2 * radius);
-            double totalX = width - 2 * startX;
-            double totalY = height - 2 * startY;
-            if (totalX < 2 * radius || totalY < 2 * radius) {
-                IJ.log("Invalid radius");
-                return;
+    private void processImage(ImageProcessor ip, long seed, boolean useAverage) throws OutOfAreaException {
+        LocationGenerator loc = new LocationGenerator(seed, radius, numbers.length);
+        int width = ip.getWidth();
+        int height = ip.getHeight();
+        double startX = Math.max(width / 20, 2 * radius);
+        double startY = Math.max(height / 20, 2 * radius);
+        double totalX = width - 2 * startX;
+        double totalY = height - 2 * startY;
+        if (totalX < 2 * radius || totalY < 2 * radius) {
+            IJ.log("Invalid radius");
+            return;
+        }
+        if (ip instanceof ShortProcessor) {
+            ShortProcessor processor = (ShortProcessor) ip;
+            short[] px = (short[]) processor.getPixels();
+            for (int k = 0; k < numbers.length; k++) {
+                double sum = 0;
+                double count = 0;
+                loc.generate(startX, totalX, startY, totalY);
+                double xx = loc.x;
+                double yy = loc.y;
+                for (int x = (int) (xx - radius) - 1; x < (int) (xx + radius) + 1; x++) {
+                    for (int y = (int) (yy - radius) - 1; y < (int) (yy + radius) + 1; y++) {
+                        double dx = xx - (double) x;
+                        double dy = yy - (double) y;
+                        double d = Math.sqrt(dx * dx + dy * dy);
+                        if (d <= radius + 0.001) {
+                            double value = averages[k];
+                            if (!useAverage) {
+                                int valueInt = (int) px[x + y * width] & 0xFFFF;
+                                value = (double) valueInt + numbers[k];
+                                if (value < 0.0) {
+                                    value = 0;
+                                }
+                                if (value >= 65535.0) {
+                                    value = 65535.0;
+                                }
+                            }
+                            px[x + y * width] = (short) (int) (value + 0.5);
+                            sum += value;
+                            count++;
+                        }
+                    }
+                }
+                averages[k] = sum / count;
             }
-            if (ip instanceof ShortProcessor) {
-                ShortProcessor processor = (ShortProcessor) ip;
-                short[] px = (short[]) processor.getPixels();
-                for (int k = 0; k < numbers.length; k++) {
-                    double sum = 0;
-                    double count = 0;
-                    double xx = Math.round(startX + totalX * rnd.nextDouble());
-                    double yy = Math.round(startY + totalY * rnd.nextDouble());
-                    for (int x = (int)(xx - radius) - 1; x < (int)(xx + radius) + 1; x++) {
-                        for (int y = (int)(yy - radius) - 1; y < (int)(yy + radius) + 1; y++) {
-                            double dx = xx - (double)x;
-                            double dy = yy - (double)y;
-                            double d = Math.sqrt(dx * dx + dy * dy);
-                            if (d <= radius + 0.001) {
-                                double value = averages[k];
-                                if (!useAverage) {
-                                    int valueInt = (int)px[x + y * width] & 0xFFFF;
-                                    value = (double)valueInt + numbers[k];
-                                    if (value < 0.0) value = 0;
-                                    if (value >= 65535.0) value = 65535.0;
+        } else if (ip instanceof ByteProcessor) {
+            ByteProcessor processor = (ByteProcessor) ip;
+            byte[] px = (byte[]) processor.getPixels();
+            for (int k = 0; k < numbers.length; k++) {
+                double sum = 0;
+                double count = 0;
+                loc.generate(startX, totalX, startY, totalY);
+                double xx = loc.x;
+                double yy = loc.y;
+                for (int x = (int) (xx - radius) - 1; x < (int) (xx + radius) + 1; x++) {
+                    for (int y = (int) (yy - radius) - 1; y < (int) (yy + radius) + 1; y++) {
+                        double dx = xx - (double) x;
+                        double dy = yy - (double) y;
+                        double d = Math.sqrt(dx * dx + dy * dy);
+                        if (d <= radius + 0.001) {
+                            double value = averages[k];
+                            if (!useAverage) {
+                                int valueInt = (int) px[x + y * width] & 0xFF;
+                                value = (double) valueInt + numbers[k];
+                                if (value < 0.0) {
+                                    value = 0;
                                 }
-                                px[x + y * width] = (short)(int)(value + 0.5);
-                                sum += value;
-                                count++;
-                            }
-                        }                    
-                    }
-                    averages[k] = sum / count;
-                }
-            } else if (ip instanceof ByteProcessor) {
-                ByteProcessor processor = (ByteProcessor) ip;
-                byte[] px = (byte[]) processor.getPixels();
-                for (int k = 0; k < numbers.length; k++) {
-                    double sum = 0;
-                    double count = 0;
-                    double xx = startX + totalX * rnd.nextDouble();
-                    double yy = startY + totalY * rnd.nextDouble();
-                    for (int x = (int)(xx - radius) - 1; x < (int)(xx + radius) + 1; x++) {
-                        for (int y = (int)(yy - radius) - 1; y < (int)(yy + radius) + 1; y++) {
-                            double dx = xx - (double)x;
-                            double dy = yy - (double)y;
-                            double d = Math.sqrt(dx * dx + dy * dy);
-                            if (d <= radius + 0.001) {
-                                double value = averages[k];
-                                if (!useAverage) {
-                                    int valueInt = (int)px[x + y * width] & 0xFF;
-                                    value = (double)valueInt + numbers[k];
-                                    if (value < 0.0) value = 0;
-                                    if (value >= 255.0) value = 255.0;
+                                if (value >= 255.0) {
+                                    value = 255.0;
                                 }
-                                px[x + y * width] = (byte)(int)(value + 0.5);
-                                sum += value;
-                                count++;
                             }
-                        }                    
+                            px[x + y * width] = (byte) (int) (value + 0.5);
+                            sum += value;
+                            count++;
+                        }
                     }
-                    averages[k] = sum / count;
                 }
-            } else if (ip instanceof FloatProcessor) {
-                FloatProcessor processor = (FloatProcessor) ip;
-                float[] px = (float[]) processor.getPixels();
-                for (int k = 0; k < numbers.length; k++) {
-                    double sum = 0;
-                    double count = 0;
-                    double xx = startX + totalX * rnd.nextDouble();
-                    double yy = startY + totalY * rnd.nextDouble();
-                    for (int x = (int)(xx - radius) - 1; x < (int)(xx + radius) + 1; x++) {
-                        for (int y = (int)(yy - radius) - 1; y < (int)(yy + radius) + 1; y++) {
-                            double dx = xx - (double)x;
-                            double dy = yy - (double)y;
-                            double d = Math.sqrt(dx * dx + dy * dy);
-                            if (d <= radius + 0.001) {
-                                double value = averages[k];
-                                if (!useAverage) {
-                                    value = (double)px[x + y * width] + numbers[k];
-                                }
-                                px[x + y * width] = (float)value;
-                                sum += value;
-                                count++;
-                            }
-                        }                    
-                    }
-                    averages[k] = sum / count;
-                }
+                averages[k] = sum / count;
             }
+        } else if (ip instanceof FloatProcessor) {
+            FloatProcessor processor = (FloatProcessor) ip;
+            float[] px = (float[]) processor.getPixels();
+            for (int k = 0; k < numbers.length; k++) {
+                double sum = 0;
+                double count = 0;
+                loc.generate(startX, totalX, startY, totalY);
+                double xx = loc.x;
+                double yy = loc.y;
+                for (int x = (int) (xx - radius) - 1; x < (int) (xx + radius) + 1; x++) {
+                    for (int y = (int) (yy - radius) - 1; y < (int) (yy + radius) + 1; y++) {
+                        double dx = xx - (double) x;
+                        double dy = yy - (double) y;
+                        double d = Math.sqrt(dx * dx + dy * dy);
+                        if (d <= radius + 0.001) {
+                            double value = averages[k];
+                            if (!useAverage) {
+                                value = (double) px[x + y * width] + numbers[k];
+                            }
+                            px[x + y * width] = (float) value;
+                            sum += value;
+                            count++;
+                        }
+                    }
+                }
+                averages[k] = sum / count;
+            }
+        }
     }
 
     private String[] splitParams(String p) {
@@ -192,6 +209,63 @@ public class Points_Generator implements PlugIn {
             arr[i] = arr[i].replace(",", ".").trim();
         }
         return arr;
+    }
+
+    private class LocationGenerator {
+
+        public double x;
+        public double y;
+        private final double radius;
+        private final Random rnd;
+        private final double[] oldX;
+        private final double[] oldY;
+        private int oldCount;
+
+        public LocationGenerator(long seed, double radius, int maxCount) {
+            this.radius = radius;
+            rnd = new Random(seed);
+            oldX = new double[maxCount];
+            oldY = new double[maxCount];
+            oldCount = 0;
+        }
+
+        public void generate(double startX, double totalX, double startY, double totalY) throws OutOfAreaException {
+            int counter = 0;
+            boolean ok;
+            do {
+                ok = true;
+                x = Math.round(startX + totalX * rnd.nextDouble());
+                y = Math.round(startY + totalY * rnd.nextDouble());
+                for (int i = 0; i < oldCount; i++) {
+                    double dx = oldX[i] - x;
+                    double dy = oldY[i] - y;
+                    double d = Math.sqrt(dx * dx + dy * dy);
+                    if (d <= 2.0 * radius + 1.0) {
+                        ok = false;
+                        break;
+                    }
+                }
+                counter++;
+            } while (!ok && counter <= 100);
+
+            if (ok) {
+                oldX[oldCount] = x;
+                oldY[oldCount] = y;
+                oldCount++;
+            } else {
+                throw new OutOfAreaException("Cannot fit!");
+            }
+        }
+    }
+
+    private class OutOfAreaException extends Exception {
+
+        public OutOfAreaException() {
+        }
+
+        public OutOfAreaException(String message) {
+            super(message);
+        }
     }
 
 }
